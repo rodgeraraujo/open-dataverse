1a7da4001853142c664c754039bb7bbc5f3fc6ec
rodgeraraujo <rogerio.araujo@mail.com>
2019-11-08 10:13:05 -0300

    Filtering data with pandas

--- app.py
+++ app.py
@@ -10,5 +10,22 @@ data.head()
 # data.set_index("curso.nome", inplace = True) 
 
 data.query('curso == "201 - Tecnologia em Análise e Desenvolvimento de Sistemas - Cajazeiras (CAMPUS CAJAZEIRAS)"', inplace = True)
+data.query('situacao != "Cancelado"', inplace = True)
 
-print(data[:10])
+# print(data.loc[: , "matricula"])
+# print(data[[:"matricula", "curso"]])
+
+# data.loc[:,"matricula"].mean()
+# print(data.iloc[0])
+# print(data.count())
+qunt_alunos = data['matricula'].value_counts().count()
+
+try:
+    for x in range(qunt_alunos):
+        aluno = data.iloc[x,x]
+        print(aluno)
+except ValueError:
+    print("Oops!")
+
+
+# print(data[:100])
